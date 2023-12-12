e6468daf1d27535c0cf7f1b24d41ee5b7362760d
Ahmed Sbai <ahmedclubust@gmail.com>
2023-12-12 22:58:36 +0100

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ImagePreviewExample
+//
+
+import Foundation
---
+++ ios/ImagePreview.swift
@@ -0,0 +1,8 @@
+@objc(ImagePreview)
+class ImagePreview: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
