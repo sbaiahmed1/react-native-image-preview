b1ac5c6290cbde2f7bcd32adc3ef30a3d24167b7
Ahmed Sbai <ahmedclubust@gmail.com>
2023-12-13 01:25:44 +0100

    chore: fix metro issue (DrawerLayoutAndroid)
    Signed-off-by: Ahmed Sbai <ahmedclubust@gmail.com>

--- example/metro.config.js
+++ example/metro.config.js
@@ -32,6 +32,20 @@ const config = {
     }, {}),
   },
 
+  server: {
+    rewriteRequestUrl: (url) => {
+      if (!url.endsWith('.bundle')) {
+        return url;
+      }
+      // https://github.com/facebook/react-native/issues/36794
+      // JavaScriptCore strips query strings, so try to re-add them with a best guess.
+      return (
+        url +
+        '?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true'
+      );
+    }, // ...
+  }, // ...
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
