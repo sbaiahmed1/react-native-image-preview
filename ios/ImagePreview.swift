d7ede79de87d890e6f55304c36756e2e2462fb63
Ahmed Sbai <ahmedclubust@gmail.com>
2023-12-30 22:22:31 +0100

    fix: removed native unnecessary files making the library fully typescript
    Signed-off-by: Ahmed Sbai <ahmedclubust@gmail.com>

--- ios/ImagePreview.swift
+++
@@ -1,8 +0,0 @@
-@objc(ImagePreview)
-class ImagePreview: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
